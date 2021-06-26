91ff46719422130f704598d14f7c2ab12805dd86
KenshiHH <kenshihh@gmx.net>
2021-06-26 12:52:43 +0200

    .

--- downloader.py
+++ downloader.py
@@ -6,7 +6,7 @@ repository = "SuperSlicer"
 assetIndex = 3
 url = requests.get("https://api.github.com/repos/"+account+"/"+repository+"/releases/latest").json()
 filename = str(url["assets"][assetIndex]["name"])
-print("downloading: "+filename) # print filename
+print("downloading: "+filename)
 r = requests.get(str(url["assets"][assetIndex]["browser_download_url"]), allow_redirects=True)
 open(filename, 'wb').write(r.content)
 with ZipFile(filename, 'r') as zipObj:
