481509f0bbc56d34ef35c170d26494a63b8c46dd
KenshiHH <kenshihh@gmx.net>
2021-06-26 14:13:31 +0200

    add download progress

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from tqdm import tqdm
 from zipfile import ZipFile
 account = "supermerill"
 repository = "SuperSlicer"
@@ -32,10 +33,21 @@ while not bValidInt:
 
 filename = str(url["assets"][val]["name"])
 print("\n downloading: "+filename+"\n please wait....")
-r = requests.get(str(url["assets"][val]["browser_download_url"]), allow_redirects=True)
 
-open(filename, 'wb').write(r.content)
+
+response = requests.get(str(url["assets"][val]["browser_download_url"]), stream=True)
+total_size_in_bytes= int(response.headers.get('content-length', 0))
+block_size = 1024 #1 Kibibyte
+progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+with open(filename, 'wb') as file:
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        file.write(data)
+progress_bar.close()
+if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+    print("ERROR, something went wrong")
+
+print("\n trying to extract file...")    
 with ZipFile(filename, 'r') as zipObj:
    zipObj.extractall()
 os.remove(filename)
-
