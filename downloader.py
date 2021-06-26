45c4c5f83684a82b304a6dec040ce0e49c358fad
KenshiHH <kenshihh@gmx.net>
2021-06-26 12:43:49 +0200

    Add files via upload

---
+++ downloader.py
@@ -0,0 +1,14 @@
+import os
+import requests
+from zipfile import ZipFile
+account = "supermerill"
+repository = "SuperSlicer"
+assetIndex = 3
+url = requests.get("https://api.github.com/repos/"+account+"/"+repository+"/releases/latest").json()
+filename = str(url["assets"][assetIndex]["name"])
+print("downloading: "+filename) # print filename
+r = requests.get(str(url["assets"][assetIndex]["browser_download_url"]), allow_redirects=True)
+open(filename, 'wb').write(r.content)
+with ZipFile(filename, 'r') as zipObj:
+   zipObj.extractall()
+os.remove(filename)
